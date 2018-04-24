769a6f90f813f9dbe7654ab053b4275fb7ad4798
Lukas Wyon <lwyon16@student.aau.dk>
2018-04-24 13:14:00 +0200

    Add files via upload
    Main Code

---
+++ ExtractFeat.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "stdafx.h"
+
+#include <cstdio>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream> // cout
+#include <fstream> // Open files
+#include <math.h>
+
+using namespace cv;
+using namespace std;
+
+struct Fillet {
+	String name;
+	float hist_mean[3] = { 0 };						// BGR
+	double area = 0,rectangularity=0,hullarity;						// Contour area + convexity(squarity) which is contour area divided by boundrect area.
+	float width = 0, height = 0;					// of RotatedRect
+
+	
+
+	
+	Rect boundRect;									// The img is generated from the original image using this boundingRect
+	Point2f contour_center_mass;					//coordinate of contour center of mass
+	vector<Point> contour;							// Coordinates of the fillet 
+	vector<vector<Point>> bloodstain_contours, indendts;		// Coordinates of the bloodstains detected + Coordinates of the detected indents				
+	Mat img, bin;									// Only the boundingRect image from original image + Binary image of fillet							
+};
+
+class ExtractFeat
+{
+public:
+	String data_file_path = "C:\\Users\\Swapsster\\Documents\\p4\\project\\CleanCode\\CleanCode\\Data\\features.dat";
+
+	//------------Uden-For-Loop----------------------
+	void clearFileContent();
+	void displayImg(const String &name, const Mat &img);
+	void makeBinary(const Mat &img, Mat &bin);
+	//------------Nuværende-fisk----------------------
+	void getMeanHist(Fillet &fillet);
+	void getDimensions(Fillet &fillet);
+	void getBloodStains(Fillet &fillet);
+	void getIndents(Fillet &fillet);
+	void getShape(Fillet &fillet);
+
+	//------------Efter-Features-------------------------------
+	void saveFeatures(const Fillet &fillet);
+	void drawFeatures(Mat &img, const Fillet &fillet);
+	//------------Main----------------------
+	void run(vector<Mat> &images);
+};
+
