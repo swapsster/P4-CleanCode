02683a5d16fb22f0bd638ca727ad27a84b557972
Lukas Wyon <lwyon16@student.aau.dk>
2018-05-28 17:18:36 +0200

    Update ExtractFeat.h

--- ExtractFeat.h
+++ ExtractFeat.h
@@ -11,46 +11,53 @@
 #include <fstream> // Open files
 #include <math.h>
 
+
 using namespace cv;
 using namespace std;
 
 struct Fillet {
-	String name;
-	float hist_mean[3] = { 0 };						// BGR
-	double area = 0,rectangularity=0,hullarity;						// Contour area + convexity(squarity) which is contour area divided by boundrect area.
-	float width = 0, height = 0;					// of RotatedRect
+    String name;
+    double hist_mean[2] = { 0 };						// Saturation, Value
+    double area = 0;
+    double convexity = 0;						// Contour area + convexity(squarity) which is contour area divided by boundrect area.
+    double skinArea = 0;
+    double largestNotch = 0;
+    bool bloodstain = false;
+    int classification = 0; 				//bad meat = 1; bad skin =2; good meat 3; good skin=4;
+    string reason = "None";
 
-	
 
-	
-	Rect boundRect;									// The img is generated from the original image using this boundingRect
-	Point2f contour_center_mass;					//coordinate of contour center of mass
-	vector<Point> contour,hull;							// Coordinates of the fillet 
-	vector<vector<Point>> bloodstain_contours, indendts,skin_contour;		// Coordinates of the bloodstains detected + Coordinates of the detected indents				
-	Mat img, bin;									// Only the boundingRect image from original image + Binary image of fillet							
+
+    Rect boundRect;									// The img is generated from the original image using this boundingRect
+    vector<Point> contour;							// Coordinates of the fillet
+    Mat img, bin;									// Only the boundingRect image from original image + Binary image of fillet
 };
 
 class ExtractFeat
 {
 public:
-	String data_file_path = "C:\\Users\\Swapsster\\Documents\\p4\\project\\CleanCode\\CleanCode\\Data\\features.dat";
+    bool testingMode = false;
+    String data_file_path = "../data/features.dat";
+    String data_file_path_classification = "../data/classification.dat";
 
-	//------------Uden-For-Loop----------------------
-	void clearFileContent();
-	void displayImg(const String &name, const Mat &img);
-	void makeBinary(const Mat &img, Mat &bin);
-	//------------Nuværende-fisk----------------------
-	void getMeanHist(Fillet &fillet);
-	void getDimensions(Fillet &fillet);
-	void getBloodStains(Fillet &fillet);
-	void getIndents(Fillet &fillet);
-	void getShape(Fillet &fillet);
-	void getSkin(Fillet &fillet);
+    //------------Uden-For-Loop----------------------
+    void clearFileContent();
+    void displayImg(const String &name, const Mat &img);
+    void makeBinary(const Mat &img, Mat &bin);
+    //------------NuvÃ¦rende-fisk----------------------
 
-	//------------Efter-Features-------------------------------
-	void saveFeatures(const Fillet &fillet);
-	void drawFeatures(Mat &img, Fillet &fillet);
-	//------------Main----------------------
-	void run(vector<Mat> &images);
+    void Classify(const Fillet &fillet);
+
+    void getMean(Fillet &fillet);
+    void getBloodstains(Fillet &fillet);
+    void getNotches(Fillet &fillet);
+    void getShape(Fillet &fillet);
+    void getSkin(Fillet &fillet);
+
+    //------------Efter-Features-------------------------------
+    void saveFeatures(const Fillet &fillet);
+    //------------Main----------------------
+    void runTesting(vector<Mat> &images);
+    void runTraining(vector<Mat> &images);
 };
 
